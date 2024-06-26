6257cf646b1284b281bb4917368830a7fbd8add5
WhileHarold <152210770+WhileHarold@users.noreply.github.com>
2024-06-26 15:31:10 +0900

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://kbeac85213248a.user-app.krampoline.com", // Replace with your backend base URL
+  baseURL: "https://k028cdbe56b81a.user-app.krampoline.com", // Replace with your backend base URL
   headers: {
     "Content-Type": "application/json",
   },
